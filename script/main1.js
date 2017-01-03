b109cc9eb26509ed7fcb62cca89622097c66ccc4
LyricLiu <yichiliu@foxmail.com>
2017-01-03 17:32:28 -0500

    add who i am page1-2

--- script/main1.js
+++ script/main1.js
@@ -1,5 +1,37 @@
 (function() {
-	$('#menu-icon').click(function() {
+    $(document).ready(function() {
+        $('#fullpage').fullpage({
+            //events
+            //onLeave: function(index, nextIndex, direction) {},
+            anchors:['firstPage', 'secondPage', 'thirdPage'],
+            afterLoad: function(anchorLink, index) {
+                var loadedSection = $(this);
+                if(index == 1){
+                    setTimeout(function() {
+                        $("#s-line1").addClass('active');
+                    }, 200);
+                    $("#s1 p").fadeIn(1000);
+                    $("#s1 p").addClass('animated fadeInUp');
+                }
+                if(index ==2){
+                    $("#s2-upper-left").fadeIn(1000);
+                    $("#s2-upper-left").addClass('animated flipInX');
+                    setTimeout(function() {
+                        $("#s-line2").addClass('active');
+                        $("#s2-bottom-right").fadeIn(1000);
+                        $("#s2-bottom-right").addClass('animated fadeInUp');
+                    }, 300);
+                    setTimeout(function() {
+                        $("#s2 h2").fadeIn(1000);
+                        $("#s2-bottom-left").fadeIn(1000);
+                        $("#s2-bottom-left").addClass('animated fadeInUp');
+                    }, 600);
+                }
+            },
+        });
+    });
+
+    $('#menu-icon').click(function() {
         $(this).toggleClass('open');
         $("#menu-bar").toggleClass('active');
         setTimeout(function() {
@@ -8,7 +40,7 @@
             $('#menu-bar div').toggleClass('active2');
         }, 200);
     });
-	setTimeout(function() {
+    setTimeout(function() {
         $("#hello-img").hide();
         $("#hello-img1").show();
     }, 2500);
