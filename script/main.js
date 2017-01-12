da0fa4a4f9d5030d4c7d8c18b703aae76ece3e9e
LyricLiu <yichiliu@foxmail.com>
2017-01-12 10:44:07 -0500

    add statement

--- script/main.js
+++ script/main.js
@@ -27,7 +27,9 @@
                 $("#page-close").css("left", "300px");
             } else {
                 $(".begin").css("left", "calc(50% - 720px)");
-                $(".begin").css("top", "18%");
+                if($(window).height() > 800){
+                    $(".begin").css("top", "18%");
+                }
                 $("#type-text").css("width", "1180px");
                 $("#type-text").css("left", "calc(50% - 590px)");
                 $("#p-wrap0").css("width", "1440px");
